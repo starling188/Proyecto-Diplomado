f03aa7085aceb124acd271df98397ede80d94ffa
starling188 <gonzalezmiranda188@gmail.com>
2024-06-01 00:13:27 -0400

    prueba local

--- Domain/Entities/Cliente.cs
+++ Domain/Entities/Cliente.cs
@@ -3,7 +3,7 @@
 namespace Domain.Entities
 {
     public class Cliente : BaseEntity
-    {
+    {//
         public string? Nombre { get; set; }
         public string? Telefono { get; set; }
         public string? Email { get; set; }
